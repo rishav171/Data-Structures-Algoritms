caf47438bf602d4c5307525bd6536febcbc32fe0
rishavk171 <rishavk171@gmail.com>
2016-02-04 07:02:20 +0530

    check complexity

--- src/sorting_algorithms/QuickSort.java
+++ src/sorting_algorithms/QuickSort.java
@@ -1,49 +1,177 @@
 package sorting_algorithms;
 
-public class QuickSort {
-	
-	public static void main(String[] args) {
-		int[] array={3,8,6,5,33,12,67,44,77,98,34,12,56,412};
-		Quicksort(array,0,array.length-1);
-	}
+	 
 
-	
-	private static int partition(int[] array, int start, int end) {
-		int pivot_index=start;
-		int pivot=array[end];
-		
-		for(int i=0;i<end;i++){
-			if(array[i]<pivot){
-				swap(array[i],array[pivot_index]);
-				pivot_index=pivot_index+1;
-			}
-			
-		}
-		swap(array[pivot_index],array[end]);
-		return pivot_index;
-		
-		
-		
-	}
-	private static void Quicksort(int[] array, int start, int end) {
-		
-		while(start<=end){
-			int partitionindex=partition(array,start,end);
-		Quicksort(array,start,partitionindex-1);
-		Quicksort(array,partitionindex+1,end);
-		
-		
-		}
-		
-		
-	}
+	public class QuickSort {
 
-	private static void swap(int fisrtelement, int secondelement) {
-		int temp;
-		temp=secondelement;
-		secondelement=fisrtelement;
-		fisrtelement=temp;
-		
-	}
+	 
 
-}
+	    private static int []a;
+
+	    public static void main(String[] args) {
+
+	        // Get a random generated array
+
+	        a = getArray();
+
+	         
+
+	        // prints the given array
+
+	        printArray();
+
+	         
+
+	        // sort the array
+
+	        sort();
+
+	         
+
+	        System.out.println("");
+
+	         
+
+	        //prints the sorted array
+
+	        printArray();
+
+	         
+
+	    }
+
+	     
+
+	    // This method sorts an array and internally calls quickSort 
+
+	    public static void sort(){
+
+	        int left = 0;
+
+	        int right = a.length-1;
+
+	             
+
+	        quickSort(left, right);
+
+	    }
+
+	     
+
+	    // This method is used to sort the array using quicksort algorithm.
+
+	    // It takes the left and the right end of the array as the two cursors.
+
+	    private static void quickSort(int left,int right){
+
+	         
+
+	        // If both cursor scanned the complete array quicksort exits
+
+	        if(left >= right)
+
+	            return;
+
+	         
+
+	        // For the simplicity, we took the right most item of the array as a pivot 
+
+	        int pivot = a[right];
+
+	        int partition = partition(left, right, pivot);
+
+	         
+
+	        // Recursively, calls the quicksort with the different left and right parameters of the sub-array
+
+	        quickSort(0, partition-1);
+
+	        quickSort(partition+1, right);
+
+	    }
+
+	     
+
+	    // This method is used to partition the given array and returns the integer which points to the sorted pivot index
+
+	    private static int partition(int left,int right,int pivot){
+
+	        int leftCursor = left-1;
+
+	        int rightCursor = right;
+
+	        while(leftCursor < rightCursor){
+
+	                while(a[++leftCursor] < pivot);
+
+	                while(rightCursor > 0 && a[--rightCursor] > pivot);
+
+	            if(leftCursor >= rightCursor){
+
+	                break;
+
+	            }else{
+
+	                swap(leftCursor, rightCursor);
+
+	            }
+
+	        }
+
+	        swap(leftCursor, right);
+
+	        return leftCursor;
+
+	    }
+
+	     
+
+	    // This method is used to swap the values between the two given index
+
+	    public static void swap(int left,int right){
+
+	        int temp = a[left];
+
+	        a[left] = a[right];
+
+	        a[right] = temp;
+
+	    }
+
+	     
+
+	    public static void printArray(){
+
+	        for(int i : a){
+
+	            System.out.print(i+" ");
+
+	        }
+
+	    }
+
+	     
+
+	    public static int[] getArray(){
+
+	        int size=10;
+
+	        int []array = new int[size];
+
+	        int item = 0;
+
+	        for(int i=0;i<size;i++){
+
+	            item = (int)(Math.random()*100); 
+
+	            array[i] = item;
+
+	        }
+
+	        return array;
+
+	    }
+
+	 
+
+	}
