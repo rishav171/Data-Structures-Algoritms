d8d20fe5091849a0967ab0bcb8df1ab31f35168e
rishavk171 <rishavk171@gmail.com>
2016-03-03 07:41:03 +0530

    link

--- src/DS/node.java
+++ src/DS/node.java
@@ -2,24 +2,27 @@ package DS;
 
 public class node {
 	private int data;
-	private int Nextnode;
+	private node Nextnode;
 	
 	
-	public node(int data) {
-		this.data=data;
-		// TODO Auto-generated constructor stub
+	
+	
+	public node(node nextnode) {
+		Nextnode = nextnode;
 	}
 	public int getData() {
-		return this.data;
+		return data;
 	}
 	public void setData(int data) {
 		this.data = data;
 	}
-	public int getNextnode() {
+	public node getNextnode() {
 		return Nextnode;
 	}
 	public void setNextnode(node nextnode) {
-		this.Nextnode = nextnode;
+		Nextnode = nextnode;
 	}
-
+	
+	
+	
 }
