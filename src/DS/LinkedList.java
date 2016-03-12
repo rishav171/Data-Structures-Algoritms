908c7bd1c7e111dccd287e91395bb8131f96f14a
rishavk171 <rishavk171@gmail.com>
2016-03-12 20:55:17 +0530

    unable to print different nodes

--- src/DS/LinkedList.java
+++ src/DS/LinkedList.java
@@ -7,12 +7,34 @@ public class LinkedList {
 	
 	
 	public void InsertatHead(int data){
+		head=null;
 		node newnode=new node(data);
+		//newnode.setData(2);
 		newnode.setNextnode(this.head);
+		this.head=newnode;
+		System.out.println();
 		
 		
 		
 		
+		
+		
+		
+		
+	}
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		String result="{";
+		node current=this.head;
+		while(current!=null){
+			result+=current.toString()+",";
+			current=current.getNextnode();
+		}
+		result+="}";
+		return result;
 	}
 	
 }
